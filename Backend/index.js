49bf66dc698b6761e8232e2c943698f08b628368
Maxwell-Zefanya <159858205+Maxwell-Zefanya@users.noreply.github.com>
2025-05-20 20:23:37 +0700

    Fixed cloudinary not reading .env

--- Backend/index.js
+++ Backend/index.js
@@ -1,14 +1,18 @@
 const express = require('express');
-require('dotenv').config;
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Cloudinary
-const cloudinary =  require("cloudinary");
 
-// Configuration
-cloudinary.config({ 
+// NeonDB
+const http = require("http");
+const { neon } = require("@neondatabase/serverless");
+const sql = neon(process.env.DATABASE_URL);
+
+// Cloudinary
+const cloudinary = require("cloudinary").v2;
+cloudinary.config({
     cloud_name: process.env.CLOUD_NAME, 
     api_key: process.env.API_KEY, 
     api_secret: process.env.API_SECRET, 
@@ -22,12 +26,6 @@ app.use('/archive', require('./src/routes/archive.routes'));
 app.use('/forum', require('./src/routes/forum.routes'));
 app.use('/thread', require('./src/routes/thread.routes'));
 app.use('/post', require('./src/routes/post.routes'));
-
-
-// NeonDB
-const http = require("http");
-const { neon } = require("@neondatabase/serverless");
-const sql = neon(process.env.DATABASE_URL);
 
 const server = app.listen(PORT, () => {
     console.log('Run Port', PORT);
